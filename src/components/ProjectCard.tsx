99cbe833f37767724cc98ce684f369f46b6f5242
Herelius <youcefboutrig92@gmail.com>
2021-11-24 16:42:04 +0100

    Add Project title, '+' button and description pannel to ProjectCard.tsx

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,7 +1,20 @@
+import "../css/ProjectCard.css";
+
 const ProjectCard = () => {
   return (
     <>
       <h1>Card</h1>
+      <div className="card">
+        <div className="card-top">
+          <p className="project-title">Project name</p>
+          <button className="btn-project"> + </button>
+        </div>
+        <div className="card-description">
+          <p>DESCRIPTION</p>
+          <p>PROJECT MANAGER</p>
+          <p>MEMBERS</p>
+        </div>
+      </div>
     </>
   );
 };
