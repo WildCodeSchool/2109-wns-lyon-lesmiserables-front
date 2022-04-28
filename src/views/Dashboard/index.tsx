536b5a03c7e6c3700468c3861595d66c5e9020ba
Amine Benakil <amine.benakil@webcastor.fr>
2022-04-28 14:53:13 +0200

    test CI

--- src/views/Dashboard/index.tsx
+++ src/views/Dashboard/index.tsx
@@ -105,6 +105,7 @@ function Dashboard() {
             margin: "24px 16px",
             padding: 24,
             minHeight: 280,
+            
           }}
         >
           <p>Welcome {profile && profile.username}</p>
