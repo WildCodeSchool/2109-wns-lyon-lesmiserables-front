5eb8a04088ca8467aa8551e65ac22fb7337ee909
Amine Benakil <amine.benakil@webcastor.fr>
2022-04-28 15:00:15 +0200

    test CI

--- src/index.tsx
+++ src/index.tsx
@@ -17,7 +17,7 @@ const authLink = setContext((_, { headers }) => {
   return {
     headers: {
       ...headers,
-      authorization: token ? `${token}` : "",
+      authorization: token ? `${token}` : "", 
     }
   }
 });
